7dd5902d88a13ec3cc5668126a1a1acab7c3dbc9
mehanahakkim <mehanahakkim@gmail.com>
2026-09-02 17:01:53 +0530

    Create list_operations.py

---
+++ list_operations.py
@@ -0,0 +1,11 @@
+numbers = [10, 20, 30, 40, 50]
+
+print("Original list:", numbers)
+
+numbers.append(60)
+print("After adding 60:", numbers)
+
+numbers.remove(20)
+print("After removing 20:", numbers)
+
+print("Length of list:", len(numbers))
